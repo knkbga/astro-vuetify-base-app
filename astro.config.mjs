5efbc59827e8a5d84db0007abc2d6d0d5c165a21
Kanak Bagga <kanakbagga1@gmail.com>
2023-05-14 11:59:18 +0530

    chore: vuetify added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,40 @@
 import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
+import vuetifyPlugin from 'vite-plugin-vuetify';
+
+/**
+ * Vuetify integration for Astro
+ * @param {import('astro/config').Options} options
+ * @returns {import('astro/config').AstroIntegration}
+ */
+const vuetify = () => {
+  return {
+    name: 'astro-vuetify-integration',
+    hooks: {
+      'astro:config:setup': ({ updateConfig }) => {
+        updateConfig({
+          vite: {
+            ssr: {
+              noExternal: ['vuetify'],
+            },
+            plugins: [vuetifyPlugin({
+              autoImport: true,
+              treeshaking: true,
+              useIconCDN: true,
+              styles: true,
+            })],
+          },
+        })
+      },
+    },
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    vue({ appEntrypoint: '/src/app' })
+    vue({ appEntrypoint: '/src/app' }),
+    vuetify(),
   ]
 });
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import type { App } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-export default (app: App) => {
-  app
+export default function setup(app: App) {
+  const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  app.use(vuetify)
 }
